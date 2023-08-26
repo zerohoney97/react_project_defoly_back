4374da854ebfcfd76247aaeaad780653082c0908
zerohoney <zerohoney97@gmail.com>
2023-08-27 00:50:05 +0900

    asdd

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://zerohoney.com","*"],
+    origin: ["https://zerohoney.com"],
     // origin: ["http://localhost:3000"],
     credentials: true,
   })
