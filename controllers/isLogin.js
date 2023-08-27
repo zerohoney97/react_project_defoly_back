c6beaad2e2796b1ba39a1c25a04fc2fdee87b280
zerohoney <zerohoney97@gmail.com>
2023-08-27 18:28:27 +0900

    asdasd

--- controllers/isLogin.js
+++ controllers/isLogin.js
@@ -4,9 +4,11 @@ exports.isLogin = async (req, res, next) => {
   try {
     const obj = req.session;
     // console.log(req, "리퀘스트");
-    console.log(req.sessionID, "isLo");
+    // console.log(req.sessionID, "isLo");
+    console.log(req.cookies, "isLo");
+    const access_token = req.cookies["access_token"];
 
-    const access_token = obj.access_token;
+    // const access_token = obj.access_token;
 
     jwt.verify(access_token, process.env.ACCESSTOKENKEY, (err, decoded) => {
       if (err) {
