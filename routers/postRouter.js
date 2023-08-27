e0a0ed1a5dd0c9a358b6e24dcd3bf72af444d6a8
zerohoney <zerohoney97@gmail.com>
2023-08-27 18:54:05 +0900

    asdsda

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -12,7 +12,7 @@ const {Boardlikeslist,updatBoardLikes,BoarddeleteLikes} = require("../controller
 // Multer 설정
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join("..",'front', "public","imgs","userplanimg"));
+      cb(null, path.join(".","imgs","userplanimg"));
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
